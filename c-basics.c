60df8795af5fd852fdc050fad2c73ca6a7c6a68b
elvismourab <152972333+elvismourab@users.noreply.github.com>
2024-04-06 21:40:44 -0300

    Add C basic syntax (hello world)

---
+++ c-basics.c
@@ -0,0 +1,12 @@
+/* 
+In C you use functions to solve the problem
+'main' function is executed by default
+header file - stdio.h = standard input and output
+*/
+
+#include <stdio.h>
+
+int main() {
+    printf("Hello world");
+    return 0;
+}
