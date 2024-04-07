a1676c4ff385dc5de2a43f25932735483ab0cd5d
elvismourab <152972333+elvismourab@users.noreply.github.com>
2024-04-06 22:16:45 -0300

    Add conditional example + string manipulation

--- c-basics.c
+++ c-basics.c
@@ -1,26 +1,34 @@
-/* 
+/*
 In C you use functions to solve the problem
 'main' function is executed by default
 header file - stdio.h = standard input and output
+header file - string.h = necessary to manipulate strings
  */
 
 #include <stdio.h>
+#include <string.h>
 
-int printmsg(char a[]) {
-    printf("String is %s\r\n", a);
+int printmsg(char a[])
+{
+    size_t len = strlen(a);
+    if (len > 0)
+    {
+        printf("String is %s\r\n", a);
+    }
 }
 
-int main() {
+int main()
+{
     printf("Hello world");
 
     char a[6] = "hello";
     printf("\nYour string is: %s\r\n", a);
 
-/* 
-Functions
-- Parameters are optional.
-- Functions are declared before main func.
- */
+    /*
+    Functions
+    - Parameters are optional.
+    - Functions are declared before main func.
+     */
     printmsg(a);
 
     return 0;
