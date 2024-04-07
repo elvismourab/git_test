cc8ec697435bf64fbae628372caa93bc6ee6ac0d
elvismourab <152972333+elvismourab@users.noreply.github.com>
2024-04-06 22:09:38 -0300

    Add Functions example

--- c-basics.c
+++ c-basics.c
@@ -2,11 +2,26 @@
 In C you use functions to solve the problem
 'main' function is executed by default
 header file - stdio.h = standard input and output
-*/
+ */
 
 #include <stdio.h>
 
+int printmsg(char a[]) {
+    printf("String is %s\r\n", a);
+}
+
 int main() {
     printf("Hello world");
+
+    char a[6] = "hello";
+    printf("\nYour string is: %s\r\n", a);
+
+/* 
+Functions
+- Parameters are optional.
+- Functions are declared before main func.
+ */
+    printmsg(a);
+
     return 0;
 }
