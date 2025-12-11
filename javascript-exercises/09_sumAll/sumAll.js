53d1c918c7d137d6b4ef9c31f17d94f40bd1bc70
elvismourab <216804366+elvismourab@users.noreply.github.com>
2025-12-10 23:07:11 -0300

    feat: js-basics 09 solved

--- javascript-exercises/09_sumAll/sumAll.js
+++ javascript-exercises/09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
-
+const sumAll = function (x, y) {
+    if (x < 0 || y < 0) return 'ERROR';
+    if (typeof x !== 'number' || typeof y !== "number") return 'ERROR';
+    if (!Number.isInteger(x) || !Number.isInteger) return 'ERROR';
+    let sum = 0;
+    let smallNum = (x < y) ? x : y;
+    let bigNum = (x > y) ? x : y;
+    for (let i = smallNum; i <= bigNum; i++) {
+        sum += i;
+    }
+    return sum;
 };
 
 // Do not edit below this line
