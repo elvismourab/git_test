6800fc2cb524b68287dd0f3db5ebc49c9d11516b
elvismourab <216804366+elvismourab@users.noreply.github.com>
2026-01-01 22:04:59 -0300

    feat: solves 13_palindromes

--- javascript-exercises/13_palindromes/palindromes.js
+++ javascript-exercises/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    const formatedWord = word.trim().toLowerCase().replace(/[^a-zA-Z0-9]/g, '');
+    const reverseWord = Array.from(formatedWord).reverse().join('');
+    return formatedWord === reverseWord;
 };
 
 // Do not edit below this line
