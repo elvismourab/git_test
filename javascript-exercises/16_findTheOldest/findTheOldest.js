861c6b7c29fc6869a3cc97379203f95f956abe22
elvismourab <216804366+elvismourab@users.noreply.github.com>
2026-01-03 09:44:56 -0300

    feat: solves 16_findTheOldest

--- javascript-exercises/16_findTheOldest/findTheOldest.js
+++ javascript-exercises/16_findTheOldest/findTheOldest.js
@@ -1,4 +1,23 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+
+    const actualYear = new Date().getFullYear();
+
+    return arr.reduce((prev, current) => {
+        let currentAge = current.yearOfDeath ? (current.yearOfDeath - current.yearOfBirth) : (actualYear - current.yearOfBirth);
+
+        if (currentAge > prev.age) {
+            return {
+                name: current.name,
+                age: currentAge,
+            };
+        }
+
+        return prev;
+
+    }, {
+        name: "",
+        age: 0,
+    })
 
 };
 
