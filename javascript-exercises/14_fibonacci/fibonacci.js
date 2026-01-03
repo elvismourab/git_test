42d9672190e18d146141d41c1e9b892136b3938c
elvismourab <216804366+elvismourab@users.noreply.github.com>
2026-01-03 09:03:25 -0300

    feat: solves 14_fibonacci

--- javascript-exercises/14_fibonacci/fibonacci.js
+++ javascript-exercises/14_fibonacci/fibonacci.js
@@ -1,5 +1,15 @@
-const fibonacci = function() {
+const fibonacci = function (num) {
+    if (num < 0) {
+        return 'OOPS'
+    }
 
+    let fibonacci = [0, 1];
+
+    for (let i = 2; i <= num; i++) {
+        fibonacci[i] = (fibonacci[i - 1] + fibonacci[i - 2]);
+    }
+
+    return fibonacci[num];
 };
 
 // Do not edit below this line
